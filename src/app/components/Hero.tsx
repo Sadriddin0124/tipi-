5a78c0f8cbe26626a715135e52121f515b8ab46a
Your Name <sadriddinravshanov19@gmail.com>
2024-11-04 10:22:08 +0500

    hero image is changed

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -11,7 +11,7 @@ const Hero = () => {
     const btn = t("hero.btn")
   return (
     <header className='max-h-[577px] h-[100%] w-full relative rounded-b-[40px] overflow-hidden'>
-      <Image src={HeroImg} alt='Hero Image' className='absolute w-[100%] h-[577px] top-0 left-0 object-cover' width={500} height={400}/>
+      <Image src={HeroImg} alt='Hero Image' className='absolute w-[100%] h-[577px] top-0 left-0 object-cover' width={1400} height={600}/>
       <div className='h-[577px] w-[50%] relative z-20 flex justify-center items-center'>
         <Image src={HeroImgBlur} alt='Hero Image Blur' className='absolute z-[-1] w-auto h-[577px] top-0 left-0' width={500} height={400}/>
         <div className='flex flex-col gap-5 items-center'>
