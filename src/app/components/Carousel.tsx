8c590581adfe5a84a716071509ccbba5d7e7c53d
Your Name <sadriddinravshanov19@gmail.com>
2024-11-04 10:03:34 +0500

    hover is added for carousel

--- src/app/components/Carousel.tsx
+++ src/app/components/Carousel.tsx
@@ -19,11 +19,11 @@ const Carousel = ({data}: {data: NewsType[]}) => {
     <Slider {...settings}>
             {data.map((item, index) => (
                 <div className='px-[10px] py-[30px]' key={index}>
-                    <div key={index} className='rounded-[10px] overflow-hidden shadow-lg flex flex-col gap-[26px]'>
+                    <div key={index} className='rounded-[10px] overflow-hidden shadow-lg flex flex-col'>
                         <div className='bg-[#D9D9D9] min-h-[200px] h-full flex justify-center items-center'>
                             <Image src={item?.img} alt={`Slide ${index + 1}`} className='w-full object-cover h-[200px]' width={500} height={400}/>
                         </div>
-                        <div className=' px-[18px] pb-[40px] text_main flex flex-col gap-4'>
+                        <div className='pt-[26px] px-[18px] pb-[40px] text_main flex flex-col gap-4 ease-linear duration-200 hover:text-white hover:bg-[#404B7C]'>
                             <span className='text-[20px] font-[600]'>{item?.date}</span>
                             <p className='text-[17px] font-[600] text-center'>{item?.desc}</p>
                         </div>
