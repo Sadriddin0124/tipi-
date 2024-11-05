39aabd3cf62ae5c8c8bb572f50bf2cfdc863bdc5
Your Name <sadriddinravshanov19@gmail.com>
2024-11-05 11:58:09 +0500

    Loader added

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import Campus from '@/app/components/Campus'
 import Contact from '@/app/components/Contact'
 import Events from '@/app/components/Events'
@@ -6,11 +7,20 @@ import Hero from '@/app/components/Hero'
 import News from '@/app/components/News'
 import Pedagogue from '@/app/components/Pedagogue'
 import Statistics from '@/app/components/Statistics'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Home = () => {
+  const [loader, setLoader] = useState<boolean>(true)
+  useEffect(()=> {
+    setTimeout(() => {
+      setLoader(false)
+    }, 500);
+  },[])
   return (
     <div className=' overflow-hidden'>
+      {loader && <div className='fixed w-full min-h-screen flex items-center justify-center bg-black z-50 top-0'>
+        <span className="loader"></span>
+      </div>}
       <Hero/>
       <Statistics/>
       <Campus/>
