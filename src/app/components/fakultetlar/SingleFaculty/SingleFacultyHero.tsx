5dbe9ad74239be9a23acc973ab99b3602a604437
Your Name <sadriddinravshanov19@gmail.com>
2024-11-25 11:17:47 +0500

    egfd

--- src/app/components/fakultetlar/SingleFaculty/SingleFacultyHero.tsx
+++ src/app/components/fakultetlar/SingleFaculty/SingleFacultyHero.tsx
@@ -66,7 +66,7 @@ console.log(title);
   }, [id, locale]);
   const FixedItem = () => {
     return <div className='md:absolute left-0 bottom-0  w-full gap-4 md:gap-14 h-full p-4 md:p-14 flex flex-col items-center md:items-start justify-end'>
-      <h2 className='font-[500] text-[24px] md:text-[32px] max-w-[300px] text-black md:text-white'>{title}</h2>
+      <h2 className='font-[500] text-[24px] md:text-[32px] text-center max-w-[300px] text-black md:text-white'>{title}</h2>
       <BtnWhite value={t("hero.btn")} />
     </div>
   }
--- src/app/components/fakultetlar/SingleFaculty/directions/DirectionsHero.tsx
+++ src/app/components/fakultetlar/SingleFaculty/directions/DirectionsHero.tsx
@@ -25,7 +25,7 @@ const DirectionsHero = () => {
   ]
   const FixedItem = () => {
     return <div className='md:absolute left-0 bottom-0  w-full h-full p-3 md:p-14 hidden md:flex items-end md:items-center justify-center md:justify-start'>
-              <h2 className='font-[500] text-[24px] md:text-[32px] text-black md:text-white'>{t("directions.title")}</h2>
+              <h2 className='font-[500] text-[24px] md:text-[32px] text-center text-black md:text-white'>{t("directions.title")}</h2>
           </div>
   }
   return (
