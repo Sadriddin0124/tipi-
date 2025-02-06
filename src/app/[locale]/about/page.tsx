0c6be090ad746915bbd904e2750b633dd9f009f8
Your Name <sadriddinravshanov19@gmail.com>
2025-02-06 14:27:28 +0500

    sd

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -5,6 +5,7 @@ import AboutFaculty, {
 } from "@/app/components/fakultetlar/SingleFaculty/AboutFaculty";
 import { fetchBlog, fetchVideoSlider } from "@/app/lib/actions";
 import { AboutSliderType } from "@/app/types/all.types";
+import { useTranslations } from "next-intl";
 import Image from "next/image";
 import { useSearchParams } from "next/navigation";
 import React, { useEffect, useState } from "react";
@@ -18,26 +19,26 @@ const Constructor = () => {
   const groups = searchparams.get("groups");
   const [popUp, setPopUp] = useState<string>("");
   const [activeTab, setActiveTab] = useState("1");
-
-  const idLength = id?.split("").length
-  
+  const t = useTranslations();
 
   const items = [
     {
       id: "1",
-      label: "Salom",
+      label: t("information.item1"),
     },
     {
       id: "2",
-      label: "Salom2",
-    },  
+      label: t("information.item2"),
+    },
   ];
 
   useEffect(() => {
     const getData = async () => {
       const slider = await fetchVideoSlider(id);
       setSlider(slider);
-      const blog = await fetchBlog(`about-${id}${activeTab === "2" && groups ? `-2` : ""}`);
+      const blog = await fetchBlog(
+        `about-${id}${activeTab === "2" && groups ? `-2` : ""}`
+      );
       setBlog(blog);
     };
     getData();
@@ -68,20 +69,26 @@ const Constructor = () => {
         </div>
       )}
       {slider?.file && <ConstructorSlider data={slider} />}
-     {groups && <div className="pt-5 grid grid-cols-2 gap-2 max-w-[600px] w-full">
-        {items?.map((item, index) => (
-          <button
-          onClick={()=>setActiveTab(item?.id)}
-            key={index}
-            className="ease-linear group duration-200 md:bg-[#404B7C] shadow-md shadow-gray-500 w-full md:w-auto whitespace-nowrap group px-4 md:px-12 py-3 md:py-6 text-white cursor-pointer text-[11px] sm:text-[16px] md:text-[20px] rounded-full md:rounded-[10px] lg:text-[24px] font-[600] justify-center flex gap-3 items-center"
-          >
-            <div className="relative flex items-center flex-col justify-center">
-              {item?.label}
-              <span className={`${item?.id === activeTab ? "w-full" : ""} ease-linear duration-200 w-0 group-hover:w-full h-[3px] bottom-0 left-0 bg-white rounded-full`}></span>
-            </div>
-          </button>
-        ))}
-      </div>}
+      {groups && (
+        <div className="pt-5 grid grid-cols-2 gap-2 max-w-[1000px] w-full">
+          {items?.map((item, index) => (
+            <button
+              onClick={() => setActiveTab(item?.id)}
+              key={index}
+              className="ease-linear group duration-200 md:bg-[#404B7C] shadow-md shadow-gray-500 w-full md:w-auto whitespace-nowrap group px-4 md:px-12 py-3 md:py-6 text-white cursor-pointer text-[11px] sm:text-[16px] md:text-[20px] rounded-full md:rounded-[10px] lg:text-[24px] font-[600] justify-center flex gap-3 items-center"
+            >
+              <div className="relative flex items-center flex-col justify-center">
+                {item?.label}
+                <span
+                  className={`${
+                    item?.id === activeTab ? "w-full" : ""
+                  } ease-linear duration-200 w-0 group-hover:w-full h-[3px] bottom-0 left-0 bg-white rounded-full`}
+                ></span>
+              </div>
+            </button>
+          ))}
+        </div>
+      )}
       <div className="pb-[40px] px-3 md:pb-[80px] mt-6 grid items-end grid-cols-1 lg:grid-cols-2 gap-3 max-w-[1400px] w-full">
         {blog?.map((item, index) => (
           <div
